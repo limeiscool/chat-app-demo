23a4e06e81b34e41116c1c8bee7190bfdd29232c
Richard Marshall <rich.qazwsx@gmail.com>
2024-02-06 01:43:22 -0800

    chore: css/deps pass.socket depres

--- routes.js
+++ routes.js
@@ -3,6 +3,7 @@ module.exports = function (app, myDataBase, path) {
     res.sendFile(path.join(__dirname, "public", "index.html"));
   });
   app.route("/signup").get((req, res) => {
-    res.sendFile(path.join(__dirname, "public", "register.html"));
+    res.sendFile(path.join(__dirname, "public", "signup.html"));
   });
+  app.route("/login").get((req, res) => {});
 };
